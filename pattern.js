ac477062ba4f7761fab763da88010b99ea5c716c
Saikiran5000 <154650654+Saikiran5000@users.noreply.github.com>
2026-03-14 11:54:24 +0530

    Create pattern.js
    Added  diamond pattern using nested loops

---
+++ pattern.js
@@ -0,0 +1,44 @@
+let n = 5;
+
+// Upper part
+for (let i = 1; i <= n; i++) {
+
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    for (let j = 1; j <= (2 * i - 1); j++) {
+
+        if (j == 1 || j == (2 * i - 1)) {
+            row += "*";
+        } else {
+            row += " ";
+        }
+
+    }
+
+    console.log(row);
+}
+
+for (let i = n - 1; i >= 1; i--) {
+
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    for (let j = 1; j <= (2 * i - 1); j++) {
+
+        if (j == 1 || j == (2 * i - 1)) {
+            row += "*";
+        } else {
+            row += " ";
+        }
+
+    }
+
+    console.log(row);
+}
